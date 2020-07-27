69edc7b0b3167a9ef227a976e43853b2f3fd00d0
Santhosh kumar <santhosh.kumar4@tesco.com>
2020-07-27 12:32:56 +0100

    Created models and fixes issue in using local database

--- sampleapp/sampleapp/settings.py
+++ sampleapp/sampleapp/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app1',
+    'app1'
 ]
 
 MIDDLEWARE = [
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'sampleapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sys',
+        'USER': 'root',
+        'PASSWORD': 'Santhu#143',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
